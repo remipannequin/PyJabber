6e51df10f44dcd993d4e67c6258e228ec6de18b8
Rémi Pannequin <remi.pannequin@univ-lorraine.fr>
2026-03-26 12:35:08 +0100

    handshake-based authentication

--- pyjabber/network/parsers/XMLComponentParser.py
+++ pyjabber/network/parsers/XMLComponentParser.py
@@ -1,7 +1,12 @@
+from uuid import uuid4
+from xml.etree import ElementTree as ET
+
 from pyjabber.network.parsers.XMLParser import XMLParser
+from pyjabber.stream.Stream import Stream, Namespaces
+from pyjabber.stream.StreamHandler import Signal
 from pyjabber.stream.component.StanzaComponentHandler import StanzaComponentHandler
 from pyjabber.stream.component.StreamComponentHandler import StreamComponentHandler
-
+from pyjabber.utils import ClarkNotation as CN
 
 class XMLComponentParser(XMLParser):
     stanza_handler_constructor = StanzaComponentHandler
@@ -11,3 +16,47 @@ class XMLComponentParser(XMLParser):
 
     def __init__(self, host, transport, starttls):
         super().__init__(transport, starttls)
+           
+    
+    def startElementNS(self, name, qname, attrs):
+        """This is basically the same as startElementNS in XMLParser,
+        but we capture the stream id that is sent to the client"""
+        if self._stack:  # "<stream:stream>" tag already present in the data stack
+            elem = ET.Element(
+                CN.clarkFromTuple(name),
+                attrib={
+                    CN.clarkFromTuple(key): item for key,
+                    item in dict(attrs).items()})
+            self._stack.append(elem)
+
+        elif name[1] == "stream" and name[0] == "http://etherx.jabber.org/streams":
+            elem = ET.Element(
+                CN.clarkFromTuple(name),
+                attrib={
+                    CN.clarkFromTuple(key): item for key,
+                    item in dict(attrs).items()})
+            self._from_claim = elem.attrib.get("from")
+            self._stack.append(elem)
+
+            # we nned to know the id to verify handshake
+            id = str(uuid4())
+            to = attrs.get((None, "to"))
+            rsp_stream = Stream(
+                id=id,
+                from_=to,
+                to=attrs.get((None, "from")),
+                version=attrs.get((None, "version"), "1.0"),
+                xml_lang=attrs.get(("http://www.w3.org/XML/1998/namespace", "lang")),
+                xmlns=Namespaces.COMPONENT.value
+            )
+
+            self._streamHandler.set_stream_id(id, to)
+            self._transport.write(rsp_stream.open_tag())
+                
+            signal = self._streamHandler.handle_open_stream()
+            if signal and signal == Signal.DONE:
+                self._stanzaHandler = self.stanza_handler_constructor(self._transport)
+                self._state = self.StreamState.READY
+
+        else:
+            raise Exception()
